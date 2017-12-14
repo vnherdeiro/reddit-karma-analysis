d2216306139887e74a36933887d36b04bf7ac614
giim <victor.oriedreh@gmail.com>
2017-12-14 00:14:40 +0000

    migrated to python3

--- karma_distrib.py
+++ karma_distrib.py
@@ -1,17 +1,17 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 
 import re
-import urllib2
-from Queue import Queue
+import urllib
+import urllib.request
+from queue import Queue
+#from bs4 import BeautifulSoup as bs
 import pickle
 from time import sleep
-from socket import gethostname
 from random import choice
 from sys import argv
 
 #update this to python3 plus parsing with BS4
 
-hostname = gethostname()
 infile = argv[1]
 outfile = infile
 #CriticalMass = 10000 #upper boundary on the user Queue mass
@@ -20,14 +20,11 @@ baseUrl = "http://www.reddit.com/user/"
 users = Queue()
 userSet = set()
 
-#with open(infile,"r") as f:
-#	results = pickle.load(f)
 with open(infile,"r") as f:
 	dat = [line.rstrip("\n").split() for line in f.readlines()]
-results = {user:int(karma) for user, karma in dat}
+results = {user : int(karma) for user, karma in dat}
 del dat
-user0 = choice(results.keys())
-print user0
+user0 = choice(list(results.keys()))
 users.put(user0)
 userSet |= set(results.keys())
 
@@ -35,34 +32,31 @@ userSet |= set(results.keys())
 while not users.empty():
 	user = users.get()
 	try:
-		request = urllib2.Request( baseUrl + user, headers= {'User-agent': 'Mozilla/5.0'})
-		raw_dat = urllib2.urlopen( request).read()
-	except:
-		print "\t\trejection..."
-		sleep(5)
-	#read karma here
-	else:
+		opener = urllib.request.build_opener()
+		opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+		data = opener.open(baseUrl + user)
+		raw_dat = data.read().decode("utf-8") 
 		ind = raw_dat.find("comment-karma")
 		substr = raw_dat[ind:ind+60]
 		substr = substr[substr.find(">")+1:substr.find("<")]
 		karma = "".join([_ for _ in substr if _.isdigit()])
 		karma = int(karma)
-		print user,"\t",karma
+		print( user,"\t",karma)
 		results[user] = karma
 		with open(outfile,"a") as f:
 			f.write("%s %d\n" %(user,karma))
-
 		new_users = re.findall("user\/(\w+)", raw_dat)
-		#new_users = map(lambda _ : _[5:],new_users)
 		new_users = {_ for _ in new_users if _ not in userSet}
 		userSet |= new_users
-		#while new_users and users.qsize() <= CriticalMass:
-		#	users.put(new_users.pop()) #NOT WORKING BECAUSE WE ADDED ALL THE USERS TO THE SET
-			#IMPLEMENT IF REALLY NEEDED (QUEUE GROWS TOO LARGE)
 		for _ in new_users:
 			users.put(_)
-	#sleep(1) #TRYING NO DELAY THIS TIME
+	except:
+		print( "\t\trejection...")
+		sleep(5)
+	#read karma here
+	else:
+		sleep(2)
 
-print "Queue emptied!"
+print ("Queue emptied!")
 
 #EOF
